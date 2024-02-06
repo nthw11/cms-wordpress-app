aa5c81aa121136ca3e4251a0778e6d1c8853211a
Nathaniel <nathaniel.wallace@gmail.com>
2024-02-05 19:12:18 -0500

    Initial setup

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `);
+  `)
 }
 
 const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
-);
+  process.env.WORDPRESS_API_URL
+)
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -15,10 +15,17 @@ module.exports = {
     remotePatterns: [
       {
         protocol: protocol.slice(0, -1),
-        hostname,
+        hostname: process.env.WP_IMAGES_URL,
         port,
         pathname: `${pathname}/**`,
       },
+      // {
+      //   protocol: 'https',
+      //   hostname: process.env.WP_IMAGES_URL,
+      //   port,
+      //   pathname: `${pathname}/**`        
+      // },
     ],
+    domains: [process.env.WP_IMAGES_URL, '0.gravatar.com'],
   },
-};
+}
